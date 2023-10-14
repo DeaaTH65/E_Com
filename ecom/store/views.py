e3bc85041ffef6440f9329200b046ae98d544a7c
Deaath65 <deaath65@gmail.com>
2023-10-14 09:35:21 +0545

    forms.py

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 
 def home(request):
@@ -36,4 +40,5 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html')
+    form = SignUpForm
+    return render(request, 'register.html', {'form':form})
