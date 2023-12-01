91e19ace7d14bdcc39e0f57ff46e3e0b472e6f32
Deaath65 <deaath65@gmail.com>
2023-12-01 10:15:09 +0545

    problem fixed

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -3,19 +3,43 @@ from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
 
-
-
-# Create your views here.
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+	# Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
+
+
 
 
 def cart_add(request):
+	# Get the cart
 	cart = Cart(request)
+	# test for POST
 	if request.POST.get('action') == 'post':
+		# Get stuff
 		product_id = int(request.POST.get('product_id'))
+		product_qty = int(request.POST.get('product_qty'))
+
+		# lookup product in DB
 		product = get_object_or_404(Product, id=product_id)
-		cart.add(product=product)
+		
+		# Save to session
+		cart.add(product=product, quantity=product_qty)
+
+		# Get Cart Quantity
 		cart_quantity = cart.__len__()
+
+		# Return resonse
+		# response = JsonResponse({'Product Name: ': product.name})
 		response = JsonResponse({'qty': cart_quantity})
 		return response
+
+
+def cart_delete(request):
+	pass
+
+
+def cart_update(request):
+	pass
--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
@@ -8,5 +9,5 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('product/<int:pk>', views.product, name='product'),
-    path('category/<str:ct>', views.category, name='category'),
+    path('category/<str:foo>', views.category, name='category'),
 ]
